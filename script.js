a3f58c9ad06e731541d6cde706ddff975b3a3cea
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-11-28 12:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let currentSubarray = [];
+
+  for (const num of arr) {
+    // If adding the current element to the current subarray keeps the sum within the limit
+    if (currentSubarray.reduce((sum, el) => sum + el, 0) + num <= n) {
+      currentSubarray.push(num);
+    } else {
+      // Start a new subarray with the current element
+      result.push([...currentSubarray]);
+      currentSubarray = [num];
+    }
+  }
+
+  // Add the last subarray to the result
+  result.push([...currentSubarray]);
+
+  return result;
 };
 
 const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+alert(JSON.stringify(divide(arr, parseInt(n))));
+
